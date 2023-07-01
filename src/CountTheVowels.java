f6b3bbb110e8718d24bb617d6a375848dac80a30
mathewpeedikayil <mathewphilip.peedikayil@gmail.com>
2023-07-01 17:45:12 +0100

    Added a feature which shows each vowel and its count

--- src/CountTheVowels.java
+++ src/CountTheVowels.java
@@ -6,14 +6,24 @@ public class CountTheVowels {
     private static final String ENTER_STRING_TXT = "Please enter a string: ";
     private static final String HAS_TXT = " has ";
     private static final String VOWELS_TXT = " vowels.";
+    private static final String TIMES_TXT = " times.";
+    private static final String A_OCCURS_TXT = "A occurs ";
+    private static final String E_OCCURS_TXT = "E occurs ";
+    private static final String I_OCCURS_TXT = "I occurs ";
+    private static final String O_OCCURS_TXT = "O occurs ";
+    private static final String U_OCCURS_TXT = "U occurs ";
 
+    private int countOfA;
+    private int countOfE;
+    private int countOfI;
+    private int countOfO;
+    private int countOfU;
     private char[] vowels = {'a', 'e', 'i', 'o', 'u'};
     private char chr;
     private String userInput;
     private String stringToCount;
     private int lengthOfString;
     private int  numberOfVowels;
-
 
     public void countVowels() {
         System.out.println(WELCOME_MSG);
@@ -32,6 +42,30 @@ public class CountTheVowels {
             }
         }
 
+        for(int j = 0; j < lengthOfString; j++) {
+            chr = stringToCount.charAt(j);
+            if(chr == vowels[0]) {
+                countOfA++;
+            }
+            if(chr == vowels[1]) {
+                countOfE++;
+            }
+            if(chr == vowels[2]) {
+                countOfI++;
+            }
+            if(chr == vowels[3]) {
+                countOfO++;
+            }
+            if(chr == vowels[4]) {
+                countOfU++;
+            }
+        }
+
         System.out.println(userInput + HAS_TXT + numberOfVowels + VOWELS_TXT);
+        System.out.println(A_OCCURS_TXT + countOfA + TIMES_TXT);
+        System.out.println(E_OCCURS_TXT + countOfE + TIMES_TXT);
+        System.out.println(I_OCCURS_TXT + countOfI + TIMES_TXT);
+        System.out.println(O_OCCURS_TXT + countOfO + TIMES_TXT);
+        System.out.println(U_OCCURS_TXT + countOfU + TIMES_TXT);
     }
 }
